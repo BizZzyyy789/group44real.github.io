7a3e97ad30f29c3dcb970f0b768d864947a4e4e2
BizZzyyy789 <122990039+BizZzyyy789@users.noreply.github.com>
2023-07-10 23:19:48 +0700

    Add files via upload

---
+++ lengthvn.js
@@ -0,0 +1,12 @@
+function Playyyys()
+{
+    const Province = document.getElementById("G2").value;
+    if(Province === "hanoi")
+    {
+         location.href = "https://bizzzyyy789.github.io/group4.github.io/hanoi.html";
+    }
+    else {
+        alert("PLease Check Your Province Again....!");
+    }
+}
+
